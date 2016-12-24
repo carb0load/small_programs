0165f4495620f3443bdd45cfd410c478191b769f
Dan <dsislo@gmail.com>
2016-12-24 13:03:56 -0500

    Saving progress, added a play_ground file to mess around with the pytz module and use to to adapt to rge csv reader.

--- time_zone.py
+++ time_zone.py
@@ -1,5 +1,9 @@
 import csv
+from pytz import timezone, common_timezones
+import pytz
+from datetime import datetime, timedelta
 
+utc = pytz.utc
 
 def csv_reader(raw_csv):
     with open(raw_csv) as filename:
@@ -18,6 +22,10 @@ def main():
     csv_file = 'raw_times.csv'
     parsed_times = csv_reader(csv_file)
     csv_writer(parsed_times)
+    us_central = timezone('US/Central')
+    fmt = "%H:%M:%S"
+
+    # print(common_timezones)  # list the common time zones
 
 if __name__ == '__main__':
     main()
