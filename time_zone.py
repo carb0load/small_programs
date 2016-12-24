b377a5ebbd537cce34358e68f54fec2d184b7c3a
Dan <dsislo@gmail.com>
2016-12-24 14:27:20 -0500

    Saving changes

--- time_zone.py
+++ time_zone.py
@@ -1,9 +1,9 @@
 import csv
-from pytz import timezone, common_timezones
 import pytz
-from datetime import datetime, timedelta
+from datetime import datetime
 
 utc = pytz.utc
+
 
 def csv_reader(raw_csv):
     with open(raw_csv) as filename:
@@ -12,20 +12,40 @@ def csv_reader(raw_csv):
             raw_time = str(row)
             print(raw_time)
             print('Length of time: ',len(raw_time))
+            if len(raw_time) == 8:
+                raw_hour = raw_time[2]
+                raw_minute = raw_time[4],raw_time[5]
+                print('Raw Hour from 8: ', raw_hour)
+                print('Raw minute from 8: ', raw_minute)
 
 
 def csv_writer(parsed_times):
     pass
 
 
+def format_time_zones(raw_time2):
+    localFormat = "%H:%M:%S"
+    raw_year = 1900  # place holder year use %Y-%m-%d if date needs to added
+    raw_mon = 1  # place holder month
+    raw_day = 1  # place holder day
+    raw_hour = 9
+    raw_min = 23
+    utcmoment_unaware = datetime(raw_year, raw_mon, raw_day, raw_hour, raw_min)
+    utcmoment = utcmoment_unaware.replace(tzinfo=pytz.utc)
+
+    timezones = ['America/Los_Angeles', 'Europe/Madrid',
+                 'America/Argentina/San_Juan', 'US/Eastern', 'UTC']
+
+    for tz in timezones:
+        localDatetime = utcmoment.astimezone(pytz.timezone(tz))
+        print(tz, '-', localDatetime.strftime(localFormat))
+
+
 def main():
     csv_file = 'raw_times.csv'
     parsed_times = csv_reader(csv_file)
     csv_writer(parsed_times)
-    us_central = timezone('US/Central')
-    fmt = "%H:%M:%S %Z%z"
-
-    # print(common_timezones)  # list the common time zones
+    # format_time_zones(parsed_time)
 
 if __name__ == '__main__':
     main()
--- time_zone_pg.py
+++ time_zone_pg.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 
 def main():
     localFormat = "%H:%M:%S"
-    raw_year = 2008  # place holder year use %Y-%m-%d if date needs to added
-    raw_mon = 9  # place holder month
-    raw_day = 2  # place holder day
+    raw_year = 1900  # place holder year use %Y-%m-%d if date needs to added
+    raw_mon = 1  # place holder month
+    raw_day = 1  # place holder day
     raw_hour = 9
     raw_min = 23
     utcmoment_unaware = datetime(raw_year, raw_mon, raw_day, raw_hour, raw_min)
